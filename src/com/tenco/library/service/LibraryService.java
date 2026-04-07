6658fec606db8d8ad12ca7a693e97539e36ca2eb
wing853 <qwqhsk@naver.com>
2026-04-07 16:46:14 +0900

    feat:libraryService ì구현

--- src/com/tenco/library/service/LibraryService.java
+++ src/com/tenco/library/service/LibraryService.java
@@ -1,4 +1,107 @@
 package com.tenco.library.service;
 
+// 비즈니스 로직을 처리하는 서비스 클래스
+// VIEW 계층(화면) -> Service 계층 --> Dat 계층
+// VIEW 계층에서는 DAO를 직접 호출 하지말고 항상 Service를 통해서 접근한다.
+
+import com.tenco.library.dao.BookDAO;
+import com.tenco.library.dao.BorrowDAO;
+import com.tenco.library.dao.StudentDAO;
+import com.tenco.library.dto.Book;
+import com.tenco.library.dto.Student;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class LibraryService {
+    private final BookDAO bookDAO = new BookDAO();
+    private final StudentDAO studentDAO = new StudentDAO();
+    private final BorrowDAO borrowDAO = new BorrowDAO();
+
+    // 도서 추가 기능(제목, 저자 필수 검증)
+    public void addBook(Book book) throws SQLException {
+        // 1. 유효성 검사
+        if (book.getTitle() == null || book.getTitle().trim().isEmpty()) {
+            throw new SQLException("도서 제목은 필수 입력 항목입니다.");
+        }
+
+        if (book.getAuthor() == null || book.getAuthor().trim().isEmpty()) {
+            throw new SQLException("도서 저자는 필수 입력 항목입니다.");
+        }
+        bookDAO.addBook(book);
+    }
+
+    // 전체 도서 목록 조회(대출 여부 상관 없이 다 출력)
+    public List<Book> getAllBoos() throws SQLException {
+        return bookDAO.getAllBooks();
+    }
+
+    // 책 제목으로 검색
+    public List<Book> searchBooksByTitle(String title) throws SQLException {
+        if (title == null || title.trim().isEmpty()) {
+            throw new SQLException("검색어를 입력해주세요");
+        }
+        return bookDAO.searchBooksByTitle(title);
+    }
+
+    // 학생 등록 기능(이름, 학번 필수 검증)
+    public void addStudent(Student student) throws SQLException {
+        if (student.getName() == null || student.getName().trim().isEmpty()) {
+            throw new SQLException("학생의 이름은 필수 항목입니다.");
+        }
+
+        if (student.getStudentId() == null || student.getStudentId().trim().isEmpty()) {
+            throw new SQLException("학생의 학번은 필수 항목입니다.");
+        }
+        studentDAO.addStudent(student);
+    }
+
+    // 전체 학생 목록 조회
+    public List<Student> getAllStudent() throws SQLException {
+        return studentDAO.getAllStudents();
+    }
+
+    /**
+     *
+     * @param studentId: String(PK 아님)
+     * @return
+     */
+    // 학번이 유효한지 조회(로그인 처리)
+    public Student authenticateStudent(String studentId) throws SQLException {
+        if (studentId == null || studentId.trim().isEmpty()) {
+            throw new SQLException("학번을 입력해주세요");
+        }
+        return studentDAO.authenticateStudent(studentId);
+    }
+
+    // 도서 대출 요청
+
+    /**
+     *
+     * @param bookId
+     * @param studentId: 학번이 아니라 PK값
+     * @throws SQLException
+     */
+    public void borrowBook(int bookId, int studentId) throws SQLException {
+        if (bookId <= 0 || studentId <= 0) {
+            throw new SQLException("유효한 도서 아이디와 학생 아이디를 입력해주세요");
+        }
+        borrowDAO.borrowBook(bookId, studentId);
+    }
+
+    /**
+     *
+     * @param bookId
+     * @param studentId: PK(학번 아님)
+     * @throws SQLException
+     */
+    // 도서 반납 처리
+    public void returnBook(int bookId, int studentId) throws SQLException {
+        if (bookId <= 0 || studentId <= 0) {
+            throw new SQLException("유효한 도서 아이디와 학생 아이디를 입력해주세요");
+        }
+        borrowDAO.returnBook(bookId,studentId);
+    }
+
+    // TODO - 관리자 기능 추가 예정
 }
